74bb186233b154999a5e1931584b7b2acddb2f61
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2024-09-26 20:47:39 +0200

    added tailwind

---
+++ .history/tailwind.config_20240926160013.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.ejs', // Adjust the path based on your template files
+    './public/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
