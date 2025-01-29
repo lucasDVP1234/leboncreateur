71ea2a763430524cc77f2dcc7a373a81e3ad12aa
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2025-01-29 19:16:13 +0700

    seo

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,27 @@ const userRoutes = require('./users');
 const creatorRoutes = require('./creators');
 
 
+router.get('/ugc', (req, res) => {
+    res.render('ugc', {
+      success_messages: req.flash('success'),
+      error_messages: req.flash('error'),
+    });
+  });
 
+router.get('/selection-ugc', (req, res) => {
+res.render('selection-ugc', {
+    success_messages: req.flash('success'),
+    error_messages: req.flash('error')
+});
+});
 
-
+router.get('/platforme-ugc', (req, res) => {
+    res.render('platformes-ugc', {
+      success_messages: req.flash('success'),
+      error_messages: req.flash('error')
+    });
+  });
+  
 // Use other routers
 router.use('/', authRoutes);
 router.use('/', userRoutes);
